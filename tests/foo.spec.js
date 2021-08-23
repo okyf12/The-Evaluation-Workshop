36b44d7101d0a1776a78f4e1091aad639642a048
Monika <monika987456321@gmail.com>
2021-08-23 17:01:59 +0300

    done

--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -12,61 +12,19 @@ test.describe('', () => {
 ); 
     test.beforeEach(async () => {
         dashboard = new dashboardPage(page);
-    });
-    test('basic test simo', async () => {
-        await dashboard.gotoDay('Antradienis');
-        
     }); 
-    test('basic test', async () => {
-        // await page.isVisible('.v-list__tile v-list__tile--link theme--light')
-        await page.goto('https://lunch.devbstaging.com');
-        const kin = await page.$('div.dish-card');
-        console.log(kin);
-
-        expect(true).toBe(true);
+    test('Test message when maximum dish amount is added', async () => {
+        await dashboard.gotoDay('Ketvirtadienis');
+        await dashboard.gotoProvider('Green tiekejas');
+        await page.click('.dish-card.flex.ma-2.v-card.v-card--hover .v-card__text');
+        for (let i = 0; i < 10; i++) {
+            await page.click('.dish-card.flex.ma-2.v-card.v-card--hover .v-card__text', { button: 'right'});
+            await page.click('text=Pridėti papildomą patiekalą');
+        }
+        const error = await page.isVisible('div.v-snack__content');
+        expect(error).toBe(true);
     });
-    test('test', async()=>{
-
-        // Click text=checkAntradienis >> :nth-match(div, 3)
-        await page.click('text=checkAntradienis >> :nth-match(div, 3)');
-        expect(page.url()).toBe('https://lunch.devbstaging.com/dishes/tuesday/Amber%20Grill');
-
-        // Click text=Cepelinai jėga
-        await page.click('text=Cepelinai jėga');
-        expect(page.url()).toBe('https://lunch.devbstaging.com/dishes/tuesday/Cepelinai%20j%C4%97ga');
-
-        // Click text=account_circle6euro_symbol1.00timercheck
-        await page.click('text=account_circle6euro_symbol1.00timercheck');
-
-        // Click text=account_circle6euro_symbol1.00timercheck
-        await page.click('text=account_circle6euro_symbol1.00timercheck', {
-            button: 'right'
-        });
-
-        // Click text=Pridėti papildomą patiekalą
-        await page.click('text=Pridėti papildomą patiekalą');
-
-        // Click text=account_circle6euro_symbol1.00timercheck
-        await page.click('text=account_circle6euro_symbol1.00timercheck', {
-            button: 'right'
-        });
-
-        // Click text=Pridėti papildomą patiekalą
-        await page.click('text=Pridėti papildomą patiekalą');
-
-        // Click text=account_circle0euro_symbol0.10
-        await page.click('text=account_circle0euro_symbol0.10', {
-            button: 'right'
-        });
-
-        // Click text=Pridėti papildomą patiekalą
-        await page.click('text=Pridėti papildomą patiekalą');
-
-        // Click button:has-text("5.10 €send")
-        await page.click('button:has-text("5.10 €send")');
-    });
-    test('tesukas ', async () => {
-        await page.goto('https://lunch.devbstaging.com/login-password');
-    });
+   
+   
 });
 
