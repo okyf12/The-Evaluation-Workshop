673c68288f51b8d42da8a2fe076f9b61f3451f59
TrapvithMind <minetraper@gmail.com>
2021-08-23 13:30:07 +0300

    added login

--- pages/lunchDBPage.js
+++ pages/lunchDBPage.js
@@ -6,7 +6,10 @@ exports.lunchDBPage = class lunchDBPage {
     }
 
     async goto() {
-        await this.page.goto('https://lunch.devbstaging.com');
+        await this.page.goto('https://lunch.devbstaging.com/login-password');
+        await this.page.fill('[aria-label="Email"]', "simas.markevicius@sourceryacademy.com" );
+        await this.page.fill('[aria-label="Password"]', "nera svarbus32" );
+        await this.page.click('.v-btn__content' );
     }
    
 }
--- tests/foo.spec.js
+++ tests/foo.spec.js
@@ -1,6 +1,13 @@
 const { test, expect } = require('@playwright/test');
 const { lunchDBPage } = require('../pages/lunchDBPage');
+test.describe('', () => {
+    let page;
+test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    login = new lunchDBPage(page);
+    await login.goto();
+}); 
+test('basic test', async ({}) => {
 
-test('basic test', async ({ page }) => {
-
+});
 });
